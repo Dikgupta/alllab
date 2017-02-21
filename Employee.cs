9b1f755d40b7d8d613ddbef692109e07ef3998d7
Dikgupta <somya.1345@gmail.com>
2017-02-21 18:32:17 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication
+{
+    public class Employee
+    {
+        int empid;
+        string name;
+        double sal;
+
+        public int EmployeeID
+        {
+            get { return empid; }
+            set { empid = value; }
+        }
+
+        public string EmployeeName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Salary
+        {
+            get { return sal; }
+            set { sal = value; }
+        }
+    }
+}
